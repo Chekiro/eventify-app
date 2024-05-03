52e32978a6261535bf0df5d5dfa6489d8831fb02
Chekiro <chekiro@wp.pl>
2024-05-03 11:41:37 +0200

    User , Event Schema

---
+++ lib/database/index.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let cashed = (global as any).mongoose || { conn: null, promise: null };
+
+export const connectToDb = async () => {
+  if (cashed.conn) return cashed.conn;
+
+  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing!");
+
+  cashed.promise =
+    cashed.promise ||
+    mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+      dbName: "eventify",
+    });
+
+  cashed.conn = await cashed.promise;
+  return cashed.conn;
+};
