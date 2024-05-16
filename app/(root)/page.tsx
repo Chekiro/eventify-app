cc78f0946dec917f476b9d9ed092f286d8252e71
Chekiro <chekiro@wp.pl>
2024-05-16 11:11:53 +0200

    Update , Search and Category Filter Added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,8 @@ import { getAllEvents } from "@/lib/actions/event.actions";
 import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
+import Search from "@/components/shared/Search";
+import CategoryFilter from "@/components/shared/CategoryFilter";
 
 export default async function Home({ searchParams }: SearchParamProps) {
   const page = Number(searchParams?.page) || 1;
@@ -51,7 +53,10 @@ export default async function Home({ searchParams }: SearchParamProps) {
         <h2 className="h2-bold">
           Let's Go <br /> Check Our Amazing Events !
         </h2>
-        <div className="flex w-full flex-col gap-5 md:flex-row"></div>
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          <Search />
+          <CategoryFilter />
+        </div>
         <Collection
           data={events?.data}
           emptyTitle="No Events Found"
--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -27,7 +27,7 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { IEvent } from "@/lib/database/models/event.model";
 import { useUploadThing } from "@/lib/uploadthing";
-import { createEvent } from "@/lib/actions/event.actions";
+import { createEvent, updateEvent } from "@/lib/actions/event.actions";
 
 type EventFormProps = {
   userId: string;
@@ -81,6 +81,28 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
         if (newEvent) {
           form.reset();
           router.push(`/events/${newEvent._id}`);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    if (type === "Update") {
+      if (!eventId) {
+        router.back();
+        return;
+      }
+
+      try {
+        const updatedEvent = await updateEvent({
+          userId,
+          event: { ...values, imageUrl: uploadedImageUrl, _id: eventId },
+          path: `/events/${eventId}`,
+        });
+
+        if (updatedEvent) {
+          form.reset();
+          router.push(`/events/${updatedEvent._id}`);
         }
       } catch (error) {
         console.log(error);
